f027b35ece31a687d97ca44e30bc22a45e61f1f8
emmanuelanene <emmanuel.anene07@gmail.com>
2025-06-18 07:22:30 +0100

    Update

--- lib/core/configs/assets/app_vectors.dart
+++ lib/core/configs/assets/app_vectors.dart
@@ -5,7 +5,7 @@ class AppVectors {
   static const String format = '.svg';
 
   // Don't be confused by the fact we didn't define {} for "$format"
-  static const String logo = '${basePath}spotify_logo$format';
+  static const String logo = '${basePath}hitplay-logo$format';
   static const String sun = '${basePath}sun$format';
   static const String moon = '${basePath}moon$format';
   static const String topPattern = '${basePath}top_pattern$format';
